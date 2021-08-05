a024b92ea67c1a284adc2042116a417413651c78
grappas <grappastudiols@gmail.com>
2021-08-05 22:35:10 +0200

    jakiśtam

--- augument.sh
+++ augument.sh
@@ -11,7 +11,14 @@ if [ ! -d ~/.local/share/nvim ]
 then
     mkdir ~/.local/share/nvim
 fi
+
+if [ ! -d ~/.config/nvim]
+then
+    mkdir ~/.config/nvim
+fi
 rm ~/.local/share/nvim/init.vim
 rm ~/.vimrc
+rm ~/.config/init.vim
 ln -s $PWD/init.vim ~/.local/share/nvim/init.vim
 ln -s $PWD/init.vim ~/.vimrc
+ln -s $PWD/init.vim ~/.config/nvim/init.vim
