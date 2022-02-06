7db34f435b584420cdbf96fe5936d1c77962cb93
grappas <grappastudiols@gmail.com>
2022-02-06 17:15:10 +0100

    przerobiony augument.sh, żeby śmigał globalnie

--- augument.sh
+++ augument.sh
@@ -1,24 +1,5 @@
 #!/bin/sh
-if [ ! -d ~/.local ]
-then
-    mkdir ~/.local
-fi
-if [ ! -d ~/.local/share ]
-then
-    mkdir ~/.local/share
-fi
-if [ ! -d ~/.local/share/nvim ]
-then
-    mkdir ~/.local/share/nvim
-fi
 
-if [ ! -d ~/.config/nvim ]
-then
-    mkdir ~/.config/nvim
-fi
-rm ~/.local/share/nvim/init.vim
-rm ~/.vimrc
-rm ~/.config/init.vim
-ln -s $PWD/init.vim ~/.local/share/nvim/init.vim
-ln -s $PWD/init.vim ~/.vimrc
-ln -s $PWD/init.vim ~/.config/nvim/init.vim
+sudo echo "source /usr/share/nvim/archlinux.vim
+$(cat init.vim)" > /etc/xdg/nvim/sysinit.vim
+sudo cat ginit.vim > /etc/xdg/nvim/ginit.vim
