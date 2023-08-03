55ac494a663988bd7b61b87ba59800fd5d89933c
grappas <grappastudiols@gmail.com>
2023-08-03 17:33:00 +0200

    another fixes

---
+++ lazynvim/lazynvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,10 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      clangd = function(_, opts)
+        opts.capabilities.offsetEncoding = { "utf-16" }
+      end,
+    },
+  },
+}
