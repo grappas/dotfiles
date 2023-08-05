655f8afbbb543b1222d62ad7fcab4e736a985fab
grappas <grappastudiols@gmail.com>
2023-08-05 13:54:30 +0200

    gitignore and mini.nvim

--- nvim/lua/config/mini.lua
+++ nvim/lua/config/mini.lua
@@ -1,7 +1,11 @@
-
-require('mini.pairs').setup()
-require('mini.ai').setup()
-require('mini.indentscope').setup()
---require('mini.jump').setup()
-require('mini.surround').setup()
-require('mini.jump2d').setup()
+return {
+	"echasnovski/mini.nvim",
+	config = function()
+		require("mini.pairs").setup()
+		require("mini.ai").setup()
+		require("mini.indentscope").setup()
+		--require('mini.jump').setup()
+		require("mini.surround").setup()
+		require("mini.jump2d").setup()
+	end,
+}
