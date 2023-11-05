1d48c3f221842389bc3400bba7569c3b99c6f047
grappas <grappastudiols@gmail.com>
2023-11-05 18:46:08 +0100

    not agagin

---
+++ lazynvim/lua/plugins/git.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        }
+    },
+    {
+        "tpope/vim-fugitive"
+    }
+}
