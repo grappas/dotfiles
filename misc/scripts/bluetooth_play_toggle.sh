795a60d7e1156201a5339ddb7f290c7f4cc588e0
grappas <grappastudiols@gmail.com>
2023-12-10 13:30:54 +0100

    change bluetoothctl behavior

--- misc/scripts/bluetooth_play_toggle.sh
+++ misc/scripts/bluetooth_play_toggle.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-#
+
+if [ -z "$(bluetoothctl devices Connected | grep 8C:7A:3D:D2:BF:A6)" ]
+then
+    bluetoothctl connect 8C:7A:3D:D2:BF:A6
+    sleep 1
+fi
+
 if [ ! -z "$(echo -e "menu player\nshow" | bluetoothctl | grep "Status: playing")" ]
 then
     echo -e "menu player\npause" | bluetoothctl | grep "Status: playing"
