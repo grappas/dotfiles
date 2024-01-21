9336bf7dd7b5a8f9ac2636cd696cf7d1379c8d9a
grappas <grappastudiols@gmail.com>
2024-01-21 17:37:14 +0100

    grimblast redux hyprshade bug mitigation

--- misc/scripts/screenshot.sh
+++ misc/scripts/screenshot.sh
@@ -2,11 +2,14 @@
 
 GRIMBLAST_DATE=$(date +'%d%m%y%H%M%S')
 
-if [ -z $(hyprshade current) ]
+HYPRSHADE_CURRENT=$(hyprshade current | grep blue)
+
+echo "$GRIMBLAST_DATE"
+if [ ! -z $HYPRSHADE_CURRENT ]
 then
-    hyprshade off $(hyprshade current) && \
-        $@ && \
-        hyprshade on $(hyprshade current)
+    hyprshade off $HYPRSHADE_CURRENT && \
+        grimblast copysave $@ ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png && pinta ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png && \
+        hyprshade on $HYPRSHADE_CURRENT
 else
-    $@
+        grimblast copysave $@ ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png && pinta ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png
 fi
