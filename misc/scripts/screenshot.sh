18ca84f49c2e4462e7a825b167f78f1b4d44325a
grappas <grappastudiols@gmail.com>
2024-01-21 22:30:27 +0100

    grimblast fixed

--- misc/scripts/screenshot.sh
+++ misc/scripts/screenshot.sh
@@ -4,12 +4,13 @@ GRIMBLAST_DATE=$(date +'%d%m%y%H%M%S')
 
 HYPRSHADE_CURRENT=$(hyprshade current | grep blue)
 
-echo "$GRIMBLAST_DATE"
+echo "HYPRSHADE_CURRENT: $HYPRSHADE_CURRENT"
 if [ ! -z $HYPRSHADE_CURRENT ]
 then
-    hyprshade off $HYPRSHADE_CURRENT && \
-        grimblast copysave $@ ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png && pinta ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png& && \
-        hyprshade on $HYPRSHADE_CURRENT
+    hyprshade toggle $HYPRSHADE_CURRENT
+    grimblast copysave $@ ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png
+    pinta ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png&
+    hyprshade toggle $HYPRSHADE_CURRENT
 else
-        grimblast copysave $@ ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png && pinta ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png&
+    grimblast copysave $@ ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png && pinta ~/Obrazy/Zrzuty\ ekranu/"$GRIMBLAST_DATE".png&
 fi
