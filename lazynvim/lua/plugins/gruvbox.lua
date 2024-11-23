df560bc6e5e1be2eefee9087b349002140b7b111
grappas <grappastudiols@gmail.com>
2024-11-23 23:48:59 +0100

    gruvbox

--- lazynvim/lua/plugins/gruvbox.lua
+++ lazynvim/lua/plugins/gruvbox.lua
@@ -28,8 +28,8 @@ return {
     opts = {
       colorscheme = "gruvbox",
     },
-    require("notify").setup({
-      background_colour = "#000000",
-    }),
+    -- require("notify").setup({
+    --   background_colour = "#000000",
+    -- }),
   },
 }
