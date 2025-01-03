a885c25718ef44ec1342f64bc9064aa51b1bb7a8
grappas <grappastudiols@gmail.com>
2025-01-03 10:19:52 +0100

    another step

--- misc/scripts/eww_toggle.sh
+++ misc/scripts/eww_toggle.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -n "$(cat ~/instalancje/dotfiles/eww/eww.yuck | grep ":exclusive true")" ]; then
-  sed -i 's/:exclusive true/:exclusive false/g' ~/instalancje/dotfiles/eww/eww.yuck
+if [ -n "$(pidof eww)" ]; then
+  killall eww
 else
-  sed -i 's/:exclusive false/:exclusive true/g' ~/instalancje/dotfiles/eww/eww.yuck
+  eww open monitor1
+  sleep 0.1
+  eww open monitor2
 fi
