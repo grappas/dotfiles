99bd7e335ad5a7f7b3b2a1158d0b5f4696dc2b85
grappas <grappastudiols@gmail.com>
2025-09-25 20:07:35 +0200

    not an init anymore

--- lazynvim/lua/plugins/mini.lua
+++ lazynvim/lua/plugins/mini.lua
@@ -1,7 +1,8 @@
 return {
-  "echasnovski/mini.nvim",
+  "nvim-mini/mini.nvim",
   config = function()
     require("mini.ai").setup()
     require("mini.indentscope").setup()
+    require("mini.jump2d").setup()
   end,
 }
